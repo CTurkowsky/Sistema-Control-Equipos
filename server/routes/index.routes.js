64bf5450bdb90fb20ff0855cf8877e3e48d2121f
CTurkowsky <91448665+CTurkowsky@users.noreply.github.com>
2022-11-22 16:35:23 -0500

    Add welcome msg

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,10 +3,13 @@ import { pool } from '../db.js';
 
 const router = Router();
 
-router.get('/ping', async(req, res) => {
-    const [rows] = await pool.query('SELECT 1 + 1 as result');
-    console.log(rows);
-    res.json(rows);
+router.get('/ping', async (req, res) => {
+  const [rows] = await pool.query('SELECT 1 + 1 as result');
+  console.log(rows);
+  res.json(rows);
+});
+router.get('/', async (req, res) => {
+  res.send('Welcome to Server');
 });
 
 export default router;
